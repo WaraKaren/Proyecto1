a6fb68950942c3a4cabdb94adf8c413d7add5be9
WaraKaren <wkpacajesm@gmail.com>
2023-07-26 15:44:05 -0400

    26/7

--- src/analyzer.js
+++ src/analyzer.js
@@ -6,8 +6,11 @@ const analyzer = {
 
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+  getCharacterCountExcludingSpaces: (text) => {
+    if (!text) {
+    return 0;
+    
+    }//TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@
 /*en este caso para que querySelector seleccione el que queremos y que no
 seleccione el primero que se le aparesca es que se utiliza los corchetes[]
 especificando que se busca un textare que tenga name=user-input*/
-const textArea = document.querySelector('[name="user-input"]');
+const userInput = document.querySelector('textarea');
 //con addEventListener se asigana una acion luego de obtner una respuesta
-textArea.addEventListener('keyup'texto)
-console.log(textArea);
+userInput.addEventListener('keyup', () => {
+const palabras= //poner funcion y poner para cada una de las li
+const
+}
+console.log(userInput);
+
 //varibles declaradas de las li
 const wordCount = document.querySelector('[data-testid="word-count"]');
 const characterCount = document.querySelector('[data-testid="character-count"]');
 const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]');
 const numberCount = document.querySelector('[data-testid="number-count"]');
 const numberSum = document.querySelector('[data-testid="number-sum"]');
-const wordLengthAverage = document.querySelector('[ata-testid="word-length-average"]');
+const wordLengthAverage = document.querySelector('[data-testid="word-length-average"]');
 //para acceder a un elemto por id se utiliza getElementById
 //reset-button es el id del boton que se utilizo en html
 const VarParaLimpiar = document.getElementById("reset-button");
