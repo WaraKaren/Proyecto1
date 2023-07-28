2f7a92ebfdee49661e5bba70b5cc273bbc4db075
WaraKaren <wkpacajesm@gmail.com>
2023-07-27 23:31:21 -0400

    2 funciones listo
    ya se puede contar palabras y caracteres

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+//texarea
+const userInput = document.querySelector('textarea');
+//li
+const wordCount = document.querySelector('[data-testid="word-count"]');
+const characterCount = document.querySelector('[data-testid="character-count"]');
+const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]');
+const numberCount = document.querySelector('[data-testid="number-count"]');
+const numberSum = document.querySelector('[data-testid="number-sum"]');
+const wordLengthAverage = document.querySelector('[data-testid="word-length-average"]');
+//boton
+const resetButton = document.getElementById("reset-button");
+//funcion para textarea utilizando la variable que le asignamos anteriormente userInput
+userInput.addEventListener('keyup', function () {
+  const savedText = userInput.value;
+  //split(/\s+/\) hara la separacion por palabras en savedText
+  const words = savedText.split(/\s+/).filter(function (aloneWord) {
+    return aloneWord !== '';
+    /*a el metodo filter se le da un funcion de !== diferencias
+    en espacios si tiene espacio es falso por que igual a vacio''
+    todo esto de almacena en la const words*/
+  });
+    //metricas de cada li
+  wordCount.textContent = `PALABRAS: ${words.length}`;
+  characterCount.textContent = `CARACTERES: ${savedText.length}`;
+  /*const diferente = savedText.filter(function(ultimo){
+    return words !=='';          no funciono*/
+});
+  characterNoSpacesCount.textContent = `CARACTERES SIN ESPACIOS: ${diferente.length}`;
+});
+
+
+
+
+
 /*document es el permiso para acceder a HTML
 ///AAAquerySelector puede acceder a un elemento por su etiqueta en este caso <textarea>*/
 //AAAAconst userInput = document.querySelector('textarea');
@@ -13,10 +47,6 @@ console.log (si la lograste ahora tienes ${newWordCount} de palabras);
 }*/
 
 
-
-
-
-
 //varibles declaradas de las li
 /*en este caso para que querySelector seleccione el que queremos y que no
 seleccione el primero que se le aparesca es que se utiliza los corchetes[]
@@ -30,23 +60,3 @@ const wordLengthAverage = document.querySelector('[data-testid="word-length-aver
 //para acceder a un elemto por id se utiliza getElementById
 //reset-button es el id del boton que se utilizo en html
 const VarParaLimpiar = document.getElementById("reset-button");*/
-
-
-//texarea
-const userInput = document.querySelector('textarea');
-//li
-const wordCount = document.querySelector('[data-testid="word-count"]');
-const characterCount = document.querySelector('[data-testid="character-count"]');
-const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]');
-const numberCount = document.querySelector('[data-testid="number-count"]');
-const numberSum = document.querySelector('[data-testid="number-sum"]');
-const wordLengthAverage = document.querySelector('[data-testid="word-length-average"]');
-//boton
-const resetButton = document.getElementById("reset-button");
-//funcion para textarea utilizando la variable que le asignamos anteriormente userInput
-userInput.addEventListener('keyup', function () {
-//.value hace que se almacene lo que el usuario ingrece en userInput
-//todo lo que se almacena se guardo en texto
-    const savedText = userInput.value;
-
-});
